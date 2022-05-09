c7b98dcf7f275101d7f0b03566a9115eb4abfe7d
joson <joson.chou@outlook.com>
2022-05-09 22:35:55 +0800

    feat: 中文

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@
  * @author: 周金顺（云天河）
  */
 
-export const APP_NAME = '水电应急培训专业数字沙盘教学系统';
+export const APP_OUTER_NAME = '水电应急培训专业数字沙盘教学系统';
+export const APP_NAME = '应急课程教学数字沙盘系统';
 export const PORT = 3000;
